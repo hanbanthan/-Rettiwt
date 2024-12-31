06b6f2049bc4f97d776d5fff7fe1bc2d12801b1e
hanbanthan <giapnguyen2004@gmail.com>
2024-12-31 17:16:05 +0700

    UDPATED

--- hooks/usePosts.ts
+++ hooks/usePosts.ts
@@ -2,8 +2,8 @@ import useSWR from 'swr';
 
 import fetcher from '@/libs/fetcher';
 
-const usePosts = ( ) => {
-    const url = `/api/posts`;
+const usePosts = ( userId?: string) => {
+    const url = userId ?`/api/posts?userId=${userId}`: `/api/posts`;
 
     const {
         data, 
